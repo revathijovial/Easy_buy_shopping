aca96b9aebc8d98de2d9b20165e328f1abe3a13c
revathijovial <revathijovial27@gmail.com>
2023-06-10 15:11:10 +0530

    login module updated

--- src/shopping/easy_buy.java
+++ src/shopping/easy_buy.java
@@ -11,10 +11,27 @@ public class easy_buy {
 		System.out.println("chrome");
 		System.out.println("url");
 		
+		System.out.println("   ");
+		
+	}
+	
+	public static void login () {
+		
+        System.out.println("======Test login module=======");
+		
+		System.out.println("valid username valid password");
+		
+		System.out.println("valid username invalid password");
+		
+		System.out.println("invalid username valid password");
+		
+		System.out.println("invalid username invalid password");
+		
 	}
 	
 	public static void main(String[] args) {
 		browser();
+		login();
 	}
 
 	
