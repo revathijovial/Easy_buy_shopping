228704497f700a98838fab6359af30d397ace9b1
revathijovial <revathijovial27@gmail.com>
2023-06-10 15:05:42 +0530

    done

---
+++ src/shopping/easy_buy.java
@@ -0,0 +1,24 @@
+package shopping;
+
+public class easy_buy {
+	
+	
+	public static void browser() {
+		
+		
+		System.out.println("====This is master =====");
+		
+		System.out.println("chrome");
+		System.out.println("url");
+		
+	}
+	
+	public static void main(String[] args) {
+		browser();
+	}
+
+	
+	
+	
+	
+}
